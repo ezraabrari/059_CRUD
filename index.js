b04763eedc0a5dd8861e80f215a43f2f7a894517
Ezra Abrari <ezraabrari@gmail.com>
2025-10-20 09:53:51 +0700

    Menambahkan Connect Database

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 let mysql = require('mysql2');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>{
-    res.send("Hello world");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-    console.log('server is running on port $(port)')
-})
+app.listen(port, () => {
+  console.log('Server is running on port ${port}');
+});
+
+let mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'ComalMelati3',
+  database: 'mahasiswa',
+  port: 3308
+});
+db.connect((err) => {
+  if (err) {
+      console.error('Error connecting to database:', err);
+      return;
+  }
+});
